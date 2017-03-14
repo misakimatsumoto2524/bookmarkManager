c8aebd11d94b038691a703b88ee6cb3d30b15941
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-03-13 17:11:35 -0700

    Time controller done

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ var bookmarkManager = angular.module('bookmarkManager', []);
 //});
 
 
-bookmarkManager.controller('TimeController', function($scope) {
-  console.log('yoyoyo');
+bookmarkManager.controller('TimeController', ['$scope', '$interval', function($scope, $interval) {
   $scope.date = new Date();
-});
+  $interval(function() {
+    $scope.date = new Date();
+  }, 1000);
+}]);
 
